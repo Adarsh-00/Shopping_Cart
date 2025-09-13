82ff8bc4f3066f9d24764940ae693ec863ff2cc4
Adarsh-00 <134307234+Adarsh-00@users.noreply.github.com>
2025-09-13 20:31:45 +0530

    Update Fruites.jsx

--- Client/src/Fruites.jsx
+++ Client/src/Fruites.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+// import React from 'react';
 
-const Fruites = () => {
-    let fruits = ['Apple', 'Mango', 'Banana', 'Grapes', 'Orange'];
-  return (
-    <div>
-      <ul>
-        {fruits.map((item, key) => (
-            <li key={key}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+// const Fruites = () => {
+//     let fruits = ['Apple', 'Mango', 'Banana', 'Grapes', 'Orange'];
+//   return (
+//     <div>
+//       <ul>
+//         {fruits.map((item, key) => (
+//             <li key={key}>{item}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
-export default Fruites;
+// export default Fruites;
